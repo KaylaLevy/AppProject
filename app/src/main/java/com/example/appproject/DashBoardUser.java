5caffe7417c70357ded6c79ded197dd7fd20d740
Kayla <Kaylalevy167@gmail.com>
2020-06-08 16:14:26 +0200

    2

--- app/src/main/java/com/example/appproject/DashBoardUser.java
+++ app/src/main/java/com/example/appproject/DashBoardUser.java
@@ -2,7 +2,11 @@ package com.example.appproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DashBoardUser extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class DashBoardUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board_user);
+
+        Button Chat = (Button) findViewById(R.id.Chatnow);
+        Chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(DashBoardUser.this, Chat.class);
+                startActivity(i);
+            }
+        });
     }
 }
